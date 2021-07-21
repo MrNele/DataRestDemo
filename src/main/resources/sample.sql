f86f1d7cccc1ba2927ae6b776325ed7d685b5f36
MrNele <nemanja1225@gmail.com>
2021-07-21 21:56:04 +0200

    Initial commit

---
+++ src/main/resources/sample.sql
@@ -0,0 +1,5 @@
+INSERT INTO CUSTOMERS VALUES('1','nemanja','nemanja1221@gmail.com');
+INSERT INTO CUSTOMERS VALUES('2','nemanja2','nemanja1222@gmail.com');
+INSERT INTO CUSTOMERS VALUES('3','nemanja3','nemanja1223@gmail.com');
+INSERT INTO CUSTOMERS VALUES('4','nemanja4','nemanja1224@gmail.com');
+INSERT INTO CUSTOMERS VALUES('5','nemanja5','nemanja1225@gmail.com');
